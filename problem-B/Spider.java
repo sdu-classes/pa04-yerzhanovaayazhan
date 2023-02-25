f627a258dfe915ddfac062ddf4da060aff570f04
Yerzhanova Ayazhan <75575004+yerzhanovaayazhan@users.noreply.github.com>
2023-02-25 16:22:49 +0600

    Update Spider.java

---
+++ problem-B/Spider.java
@@ -0,0 +1,10 @@
+public class Spider extends Animal {
+    public Spider() {
+        super(8);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Spider is eating!");
+    }
+}
