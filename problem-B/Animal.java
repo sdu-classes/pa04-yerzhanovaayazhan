6e807fc099958c26e23ceb2d45f016dc62191c2d
Yerzhanova Ayazhan <75575004+yerzhanovaayazhan@users.noreply.github.com>
2023-02-25 16:19:56 +0600

    Update Animal.java

---
+++ problem-B/Animal.java
@@ -0,0 +1,13 @@
+public abstract class Animal {
+    protected int legs;
+
+    protected Animal(int legs) {
+        this.legs = legs;
+    }
+
+    public abstract void eat();
+
+    public void walk() {
+        System.out.println("The animal is walking with " + this.legs + " legs.");
+    }
+}
