3189156d52f9dcc743074801e343d262acebfd84
Yerzhanova Ayazhan <75575004+yerzhanovaayazhan@users.noreply.github.com>
2023-02-25 16:22:01 +0600

    Create Main.java

---
+++ problem-B/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        // Fish d = new Fish();
+        // d.eat();
+
+        // try {
+        //     d.walk();
+        // } catch (UnsupportedOperationException exc) {
+        //     System.out.println(exc.getMessage());
+        // }
+
+        
+        // Cat c = new Cat("Fluffy");
+        // c.play();
+        // c.eat();
+        // c.walk();
+
+
+        Animal a = new Fish();
+        // a.setName("Java");
+
+
+        Animal e = new Spider(); 
+        Pet p = new Cat();
+    }
+}
